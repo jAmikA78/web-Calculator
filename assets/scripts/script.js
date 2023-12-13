46675a8024a9bd81ea76bee2a4cfe61054e4777a
jamika78 <ahmed.ibrahim7893@gmail.com>
2023-12-13 17:44:15 +0200

    add bitwise operators

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -21,36 +21,40 @@ function lcm(a, b) {
     return (a * b) / gcd(a, b);
 }
 
-function root(base, n) {    
+function root(base, n) {
     return base ** (1 / n);
 }
 
-function CRoot(n){
-    return root(n,3);
+function CRoot(n) {
+    return root(n, 3);
 }
-function SRoot(n){
-    return root(n,2);
+function SRoot(n) {
+    return root(n, 2);
 }
 
 function logn(base, x) {
     return Math.log(x) / Math.log(base);
 }
 
-function log(n){
-    return logn(n,10);
+function log(n) {
+    return logn(n, 10);
 }
 
-function log2(n){
-    return logn(n,2);
+function log2(n) {
+    return logn(n, 2);
+}
+
+function xnor(a, b) {
+    return ~(a ^ b);
 }
 
 function equality() {
     let expression = operation.value;
     expression = expression.replace(/pow\(/g, 'Math.pow(');
+    expression = expression.replace(/<->\(/g, 'xnor(');
     expression = expression.replace(/sin\(/g, 'Math.sin(');
     expression = expression.replace(/cos\(/g, 'Math.cos(');
     expression = expression.replace(/tan\(/g, 'Math.tan(');
-    expression = expression.replace(/log2\(/g, 'Math.log2(');
     try {
         let answer = eval(expression);
         if (isFinite(answer))
